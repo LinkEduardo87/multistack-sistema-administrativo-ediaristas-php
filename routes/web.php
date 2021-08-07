99413b9c8747941aa60329da6e15171bb8237cfd
Link <link.eduardo1287@gmail.com>
2021-08-06 22:29:11 -0300

    Commit inicial com o cadastro de serviço

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controller\ServicoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Rotas para trabalhar com serviços
+Route::get('/servicos', [App\Http\Controllers\ServicoController::class, 'index'])->name('servicos.index');
+Route::get('/servicos/create', [App\Http\Controllers\ServicoController::class, 'create'])->name('servicos.create');
+Route::post('/servicos', [App\Http\Controllers\ServicoController::class, 'store'])->name('servicos.store');
+Route::get('/servicos/{servico}/edit', [App\Http\Controllers\ServicoController::class, 'edit'])->name('servicos.edit');
+Route::put('/servicos/{servico}', [App\Http\Controllers\ServicoController::class, 'update'])->name('servicos.update');
