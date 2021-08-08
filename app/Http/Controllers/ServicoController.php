79e2e2e74e48642ac6cb089316a21b7a6b2e7486
Link <link.eduardo1287@gmail.com>
2021-08-08 16:07:14 -0300

    Mensagens de feedback e ajustamos validação

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -8,6 +8,12 @@ use App\Models\Servico;
 
 class ServicoController extends Controller
 {
+    /**
+     * Lista os serviços
+     *
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
+
     public function index()
     {
         $servicos = Servico::paginate(10);
@@ -15,10 +21,23 @@ class ServicoController extends Controller
         return view('servicos.index')->with('servicos', $servicos);
     }
 
+    /**
+     * Mostra o form vazio para criação
+     * 
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
+
     public function create()
     {
         return view('servicos.create');
     }
+
+    /**
+     * Cria um novo registro no banco de dados
+     * 
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
 
     public function store(ServicoRequest $request)
     {
@@ -26,8 +45,16 @@ class ServicoController extends Controller
 
         Servico::create($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')
+                         ->with('mensagem', 'Serviço criado com sucesso');
     }
+
+    /**
+     * Mostra o formulário para alteração
+     * 
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
 
     public function edit(int $id)
     {
@@ -35,6 +62,14 @@ class ServicoController extends Controller
 
         return view('servicos.edit')->with('servico', $servico);
     }
+
+    /**
+     * Atualiza um registro no banco de dados
+     * 
+     * @param integer $id
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
 
     public function update(int $id, ServicoRequest $request)
     {
@@ -44,7 +79,8 @@ class ServicoController extends Controller
 
         $servico->update($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')
+                         ->with('mensagem', 'Serviço atualizado com sucesso');
     }
 
 }
